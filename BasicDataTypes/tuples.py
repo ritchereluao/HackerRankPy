d6f77c29dc2d591696c3766e529aea2ca355d7a1
Ritche Reluao <34097542+ritchereluao@users.noreply.github.com>
2021-10-02 00:55:03 +0800

    Create tuples.py

---
+++ BasicDataTypes/tuples.py
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/python-tuples/problem
+
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+
+    l = []
+    for i in integer_list:
+        l.append(i)
+
+    t = tuple(l)
+    print(hash(t))
