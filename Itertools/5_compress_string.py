9750d97545568e6195eb8135e6928eeb4f7c09d2
Ritche Reluao <34097542+ritchereluao@users.noreply.github.com>
2021-10-07 01:37:07 +0800

    Update 5_compress_string.py

--- Itertools/5_compress_string.py
+++ Itertools/5_compress_string.py
@@ -3,14 +3,19 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 from itertools import groupby
 
+# stdin = input()
+# group_list = [list(g) for k, g in groupby(stdin)]
+
+# stdout = []
+# for each_item in group_list:
+#     output = []
+#     output.append(len(each_item))
+#     output.append(int(each_item[0]))
+#     stdout.append(tuple(output))
+
+# print(*stdout)
+
+
 stdin = input()
-group_list = [list(g) for k, g in groupby(stdin)]
-
-stdout = []
-for each_item in group_list:
-    output = []
-    output.append(len(each_item))
-    output.append(int(each_item[0]))
-    stdout.append(tuple(output))
-
+stdout = [(len(list(g)), int(k)) for k, g in groupby(stdin)]
 print(*stdout)
