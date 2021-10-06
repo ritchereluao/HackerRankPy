be24b91be43448cb0b02963223e83ce08d1b1227
Ritche Reluao <34097542+ritchereluao@users.noreply.github.com>
2021-10-06 15:01:34 +0800

    Create 6_intcomesinallsizes.py

---
+++ Math/6_intcomesinallsizes.py
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+stdout = (a**b) + (c**d)
+print(stdout)
